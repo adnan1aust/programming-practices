c01442c21cc212c83843e645febdd3cbb9b3b6a9
Ahmed Shahriar Adnan <adnan1aust@users.noreply.github.com>
2022-01-31 23:59:14 +0600

    Programming practice

---
+++ finalValueOfVariable.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int res = 0;
+        int len = operations.length;
+        for(int i = 0 ; i < len ; i++){
+            if(operations[i].equals("++X") || operations[i].equals("X++")){
+                res++;
+            } else {
+                res--;
+            }
+        }
+        return res;
+    }
+}
